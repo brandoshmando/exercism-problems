f07ffeeabcd8b7114c0f8d7d760557fb94146e49
brandoshmando <brancraft@gmail.com>
2014-10-13 16:53:03 -0500

    Made small change

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,10 @@
 class Hamming
   #str == strand versus the common abbreviation of string
   def self.compute(first_str, second_str)
-    hamms = 0
-    max_length = if first_str.length < second_str.length
-      first_str.length
-    else
-      second_str.length
-    end
+    min_length = [first_str.length, second_str.length].min
 
-    max_length.times do |i|
+    hamms = 0
+    min_length.times do |i|
       hamms += 1 if first_str[i] != second_str[i]
     end
     hamms
