eb97c3da4e1e03b0f5c6bcff0f430e7f7d1d9e13
brandoshmando <brancraft@gmail.com>
2014-10-13 16:05:46 -0500

    First test passing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,11 @@
 class Hamming
   #str == strand versus the common abbreviation of string
   def self.compute(first_str, second_str)
-    0
+    counter = 0
+    first_str.split('').each_with_index do |char, i|
+      counter += if char != second_str[i]
+      end
+    end
+    counter
   end
 end
