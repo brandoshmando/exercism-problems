bdb668580b7f5204174edd61b583bc0b9fe34136
brandoshmando <brancraft@gmail.com>
2014-10-13 16:06:47 -0500

    First two tests passing after first RGR

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,8 +3,7 @@ class Hamming
   def self.compute(first_str, second_str)
     counter = 0
     first_str.split('').each_with_index do |char, i|
-      counter += if char != second_str[i]
-      end
+      counter += 1 if char != second_str[i]
     end
     counter
   end
