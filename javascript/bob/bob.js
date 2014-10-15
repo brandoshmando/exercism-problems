1e3fff4773074c92c9731158073971ff75d313fc
brandoshmando <brancraft@gmail.com>
2014-10-15 15:16:49 -0500

    3 tests passing. This feels messy....

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,10 +6,12 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (input.slice(-1) == '.'){
-    return "Whatever."
+  if (input.slice(-1) == '?'){
+    return "Sure."
   }else if (input == input.toUpperCase()) {
     return "Whoa, chill out!"
+  }else if (input.slice(-1) == '.'){
+    return "Whatever."
   }
 };
 
