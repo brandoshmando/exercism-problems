7781c8d63dfa3f83b15ae030d88f9c1d972080de
brandoshmando <brancraft@gmail.com>
2014-10-19 19:54:54 -0400

    dna tests passing

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,3 +1,24 @@
 class Complement
 
+  def self.of_dna(strand)
+    complement = []
+
+    strand.each_char do |char|
+      case char
+      when "G"
+        complement << "C"
+      when "C"
+        complement << "G"
+      when "T"
+        complement << "A"
+      when "A"
+        complement << "U"
+      end
+    end
+    complement.join
+  end
+
+  def self.of_rna(strand)
+
+  end
 end
